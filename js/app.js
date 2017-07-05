367c2f29c0b7b19f3aafaa69d499c6bd0648877c
jarenmcdonald <estyn72@gmail.com>
2017-07-05 08:03:57 -0400

    Added functional hamburger nav
    - Animated hamburger nav lines
    - Nav appears when clicked; disappears when clicked again

--- js/app.js
+++ js/app.js
@@ -5,4 +5,17 @@
   		this.classList.toggle( "active" );
 	});
 
-});
+	var content = document.getElementById("menuContent");
+	var button = document.getElementById("navToggle");
+
+	button.onclick = function(e) {
+	e.preventDefault()
+		if(content.className == "open"){
+			content.className = "";
+		} else {
+			content.className = "open";
+		}
+
+	};
+
+})();
